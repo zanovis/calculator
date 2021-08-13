7fd64c5928cfb39a402d9fa82b05c68542ad2140
Noah Davis <zanovis@gmail.com>
2021-08-12 22:12:52 -0600

    minor improvements, todo list

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let currentNum = '';
 let current;
 let enteredString = "";
 let operator = '';
-let firstNum = 0;
+let firstNum;
 let result = 0;
 let prev;
 
@@ -33,33 +33,38 @@ const divide = (a, b) => {
   return a / b;
 };
 
+// 1. START FRESH IF ENTERING NUM AFTER CLICKING ENTER
+// 2. PREVENT MULTIPLE OPERATORS BEING ENTERED CONSECUTIVELY
+// 3. ROUND FLOATS
+// 4. ADD MESSAGE ABOUT DIV BY ZERO
+// 5. BACKSPACE BUTTON
+// 6. ADD KEYBOARD CAPABILITY
+
 const input = (element) => {
   current = element.target.textContent;
   const operators = ["+", "x", "-", "/"];
   if (current == "CLEAR") {
     // Clear button actions
     clear();
-  } else if (operators.includes(current)) {
+  } else if (operators.includes(current) && typeof(prev) == 'string') {
     // Operator button actions
+    operate(operator, parseFloat(firstNum), parseFloat(currentNum));
     operator = current;
-    operate(operator, parseInt(firstNum), parseInt(currentNum))
     firstNum = currentNum;
-    if (display.textContent.length > 2) { firstNum = result; }
     currentNum = '';
   } else if (current == "=") {
     // Equate button actions
-    operate(operator, parseInt(firstNum), parseInt(currentNum));
+    operate(operator, parseFloat(firstNum), parseFloat(currentNum));
     firstNum = result;
     currentNum = 0;
     currentNum = result;
   } else {
     // Numeric button actions
     currentNum = currentNum + current;
-
-
+    
   }
   prev = current;
-  updateDisplay(current, currentNum);
+  updateDisplay(current, currentNum, operators);
   console.log(`CURRENT is ${current}`);
   console.log(`firstNum is ${firstNum}`);
   console.log(`currentNum is ${currentNum}`);  
@@ -67,27 +72,37 @@ const input = (element) => {
   return current;
 };
 
-const updateDisplay = (current, currentNum) => {
+const updateDisplay = (current, currentNum, operators) => {
   display.style.animation = "none";
   enteredString = enteredString + current;
   if (current == "=") {
     display.textContent = result;
     enteredString = result;
+  } else if (operators.includes(current)) {
+    enteredString = firstNum + current;
+    display.textContent = enteredString;
   } else {
     display.textContent = enteredString;
   }
 };
 
 const operate = (operator, a, b) => {
-  if (operator == "+") {
-    result = add(a, b);
-  } else if (operator == "-") {
-    result = subtract(a, b);
-  } else if (operator == "x") {
-    result = multiply(a, b);
-  } else if (operator == "/") {
-    result = divide(a, b);
-  }
+  if (firstNum){ 
+    if (operator == "+") {
+      result = add(a, b);
+    } else if (operator == "-") {
+      result = subtract(a, b);
+    } else if (operator == "x") {
+      result = multiply(a, b);
+    } else if (operator == "/") {
+      result = divide(a, b);
+    }    
+  } else {
+    result = currentNum;
+  } 
+
+  // add floating point rounding
+  // result.toFixed(2); 
   return result;
 };
 
@@ -97,7 +112,7 @@ const clear = () => {
   displayText = originalDisplay;
   enteredString = '';
   currentNum = '';
-  firstNum = 0;
+  firstNum = '';
   result = 0;
   current = '';
 };
